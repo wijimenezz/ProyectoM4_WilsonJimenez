4f9fec8d7b745e79c69f57d3773c18991d10961d
Wilson Jimenez <wijimenezz@gmailcom>
2026-06-01 11:24:52 -0500

    feats: setupde firebase listo y creacion de variables de entorno listas, modificacion de git ignore con .env

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAPBkDoB1dL5eQXNcK8yGlX4HlU-WtXT-U",
-  authDomain: "task-manager-c6243.firebaseapp.com",
-  projectId: "task-manager-c6243",
-  storageBucket: "task-manager-c6243.firebasestorage.app",
-  messagingSenderId: "78808850376",
-  appId: "1:78808850376:web:73eca98126d17b3dd4aafd",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
