0b89079a7c8ff00f98aaad37fb0621fdf86ad37e
Wilson Jimenez <wijimenezz@gmailcom>
2026-06-01 11:07:43 -0500

    feats: Instalacion firebase y creacion de firebase.ts para conectar al proyecto

---
+++ src/services/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPBkDoB1dL5eQXNcK8yGlX4HlU-WtXT-U",
+  authDomain: "task-manager-c6243.firebaseapp.com",
+  projectId: "task-manager-c6243",
+  storageBucket: "task-manager-c6243.firebasestorage.app",
+  messagingSenderId: "78808850376",
+  appId: "1:78808850376:web:73eca98126d17b3dd4aafd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
